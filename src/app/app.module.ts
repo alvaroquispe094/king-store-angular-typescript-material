33669cea620d6639019b91edec4bd00a32d07b25
alvaroquispe094 <alvaroquispe094@gmail.com>
2023-11-20 15:53:04 -0300

    fix: clean app modules and duplicates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CardComponent } from './shared/components';
-import { PagesModule } from './presentation/pages/pages.module';
-import { LandingModule } from './presentation/landing/landing.module';
 import { DataModule } from './data/data.module';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    DataModule,
-    LandingModule,
-    PagesModule,
-    CardComponent,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTreeModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, DataModule],
   providers: [],
   bootstrap: [AppComponent],
 })
