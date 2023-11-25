cc684cacc2a97acc7556a65038a7880468d9ee2f
alvaroquispe094 <alvaroquispe094@gmail.com>
2023-11-25 16:44:06 -0300

    feat: clean importson pages module

--- src/app/presentation/pages/pages.module.ts
+++ src/app/presentation/pages/pages.module.ts
@@ -6,14 +6,9 @@ import { PagesComponent } from './pages.component';
 import { AdminPageComponent } from '../../shared/layouts';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatTreeModule } from '@angular/material/tree';
 import { PanelComponent } from './panel/panel.component';
 import { NavigationComponent, HeaderComponent } from '../../shared/components';
 
@@ -24,14 +19,9 @@ import { NavigationComponent, HeaderComponent } from '../../shared/components';
     PagesRoutingModule,
     AdminPageComponent,
     MatIconModule,
-    MatButtonModule,
     MatCardModule,
     MatGridListModule,
     MatMenuModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatTreeModule,
     NavigationComponent,
     HeaderComponent,
   ],
