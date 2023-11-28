b1f59936c0519a6aae63f22cccd4767e6afc6439
alvaroquispe094 <alvaroquispe094@gmail.com>
2023-11-27 22:43:01 -0300

    feat: Finish navigation responsive

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -2,29 +2,25 @@ import { Component, Input, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { MenuModel } from 'src/app/domain/models/menu.model';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    RouterModule,
-  ],
+  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnDestroy {
   @Input() items?: MenuModel[];
+  visible?: boolean = false;
 
   ngOnDestroy(): void {
     this.items = [];
+  }
+
+  setVisibleState(param: boolean) {
+    this.visible = param;
   }
 }
