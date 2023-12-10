ad105afcc9e24b69e8aa24cacba53f2d60941fc9
alvaroquispe094 <alvaroquispe094@gmail.com>
2023-12-10 13:45:34 -0300

    feat: Finish custom smart table

--- src/app/shared/components/smart-table/smart-table.component.ts
+++ src/app/shared/components/smart-table/smart-table.component.ts
@@ -1,51 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ProductModel } from '../../../domain/models/product.model';
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
 
 @Component({
   selector: 'app-smart-table',
@@ -61,7 +21,7 @@ const NAMES: string[] = [
   templateUrl: './smart-table.component.html',
   styleUrls: ['./smart-table.component.scss'],
 })
-export class SmartTableComponent implements AfterViewInit, OnChanges {
+export class SmartTableComponent implements OnChanges {
   displayedColumns!: string[];
   dataSource!: MatTableDataSource<ProductModel>;
 
@@ -71,25 +31,12 @@ export class SmartTableComponent implements AfterViewInit, OnChanges {
   @Input() columns!: string[];
   @Input() data!: ProductModel[];
 
-  constructor() {
-    //this.displayedColumns = this.columns;
-    // Create 100 users
-    //const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
-    //const products = this.data;
-    //console.log('products sdggg: ' + this.data[0]);
-    // Assign the data to the data source for the table to render
-    //this.dataSource = new MatTableDataSource(products);
-  }
   ngOnChanges(): void {
     this.displayedColumns = this.columns;
     const products = this.data;
-    console.log('products sdggg: ' + this.data[0]['name']);
 
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(products);
-  }
-
-  ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
@@ -103,19 +50,3 @@ export class SmartTableComponent implements AfterViewInit, OnChanges {
     }
   }
 }
-
-/** Builds and returns a new User. */
-/*function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
-}*/
